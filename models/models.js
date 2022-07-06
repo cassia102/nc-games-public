917a5e86414031956a3046062a3291b874c1564b
cassia102 <cassiasadler96@gmail.com>
2022-07-06 10:51:59 +0100

    finished adding the comment count

--- models/models.js
+++ models/models.js
@@ -16,7 +16,11 @@ exports.fetchUsers = () => {
 
 exports.fetchReviewById = (review_id) => {
   return db
-    .query(`SELECT * FROM reviews WHERE review_id = $1;`, [review_id])
+    .query(
+      `SELECT reviews.*, COUNT(comments.review_id)::INT AS comment_count FROM reviews JOIN comments ON reviews.review_id = comments.review_id WHERE reviews.review_id = $1 GROUP BY reviews.review_id
+    `,
+      [review_id]
+    )
     .then(({ rows, rowCount }) => {
       if (rowCount === 0) {
         return Promise.reject({
